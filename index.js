c22ab5855aa2ed5f04a018b5eabed9d8200daac1
Depickere Sven <sven.depickere@hotmail.com>
2022-05-21 12:36:27 +0200

    feat(): Update logic

--- index.js
+++ index.js
@@ -11,46 +11,57 @@ module.exports = (app) => {
   app.on(
     ["pull_request.opened", "pull_request.synchronize"],
     async (context) => {
-      // Creates a deployment on a pull request event
-      // Then sets the deployment status to success
-      // NOTE: this example doesn't actually integrate with a cloud
-      // provider to deploy your app, it just demos the basic API usage.
-      app.log.info(context.payload);
+        const source = 'pull_request';
 
-      // Probot API note: context.repo() => { username: 'hiimbex', repo: 'testing-things' }
-      const res = await context.octokit.repos.createDeployment(
-        context.repo({
-          ref: context.payload.pull_request.head.ref, // The ref to deploy. This can be a branch, tag, or SHA.
-          task: "deploy", // Specifies a task to execute (e.g., deploy or deploy:migrations).
-          auto_merge: true, // Attempts to automatically merge the default branch into the requested ref, if it is behind the default branch.
-          required_contexts: [], // The status contexts to verify against commit status checks. If this parameter is omitted, then all unique contexts will be verified before a deployment is created. To bypass checking entirely pass an empty array. Defaults to all unique contexts.
-          payload: {
-            schema: "rocks!",
-          }, // JSON payload with extra information about the deployment. Default: ""
-          environment: "production", // Name for the target deployment environment (e.g., production, staging, qa)
-          description: "My Probot App's first deploy!", // Short description of the deployment
-          transient_environment: false, // Specifies if the given environment is specific to the deployment and will no longer exist at some point in the future.
-          production_environment: true, // Specifies if the given environment is one that end-users directly interact with.
-        })
-      );
+        const result = await checkFiles(context, source);
 
-      const deploymentId = res.data.id;
-      await context.octokit.repos.createDeploymentStatus(
-        context.repo({
-          deployment_id: deploymentId,
-          state: "success", // The state of the status. Can be one of error, failure, inactive, pending, or success
-          log_url: "https://example.com", // The log URL to associate with this status. This URL should contain output to keep the user updated while the task is running or serve as historical information for what happened in the deployment.
-          description: "My Probot App set a deployment status!", // A short description of the status.
-          environment_url: "https://example.com", // Sets the URL for accessing your environment.
-          auto_inactive: true, // Adds a new inactive status to all prior non-transient, non-production environment deployments with the same repository and environment name as the created status's deployment. An inactive status is only added to deployments that had a success state.
-        })
-      );
+        app.log.info('result =>' + JSON.stringify(result));
+
+
+        await context.octokit.rest.pulls.createReview({
+            ...getRepoSettings(context, source),
+            event: 'REQUEST_CHANGES',
+            body: createCommentText(result)
+        });
+
+        async function checkFiles(context, source) {
+            let per_page = 100
+
+            const fileMatchRegex = new RegExp('(api\/)')
+
+            const filesThatNeedReview = [];
+
+            for await (const changedFiles of context.octokit.paginate.iterator(
+                context.octokit.pulls.listFiles,
+                {...getRepoSettings(context, source), per_page}
+            )) {
+                for (let file of changedFiles.data) {
+
+                    let fileName = file.filename;
+                    if (fileMatchRegex.test(fileName)) {
+                        filesThatNeedReview.push(fileName)
+                    }
+                }
+            }
+            return filesThatNeedReview;
+        }
+
+        function getRepoSettings(context, source) {
+            return {
+                owner: context.payload.repository.full_name.split('/')[0],
+                repo: context.payload.repository.name,
+                pull_number : context.payload[source].number
+            }
+        }
+
+        function createCommentText(result) {
+            let comment = `@DepickereSven would you be so kind to review the following code changes? \n`;
+            result.forEach(result => {
+                comment = comment.concat(`- [ ] ${result} \n`)
+            })
+            return comment.slice(0, -1);
+        }
+
     }
-  );
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+  )
 };
