3555a20ef1c75e7ea5d6f3d47f97f96e1d99ea2e
Depickere Sven <sven.depickere@hotmail.com>
2022-05-21 13:57:26 +0200

    chore(): Reformat code

--- index.js
+++ index.js
@@ -11,61 +11,61 @@ module.exports = (app) => {
   app.on(
     ["pull_request.opened", "pull_request.synchronize"],
     async (context) => {
-        const config = await context.config(`tag-someone-config.yml`);
+      const config = await context.config(`tag-someone-config.yml`);
 
-        const tagPersonName = config.personToTag;
-        let message = config['message'];
-        let regexPath = config['regexPath'];
+      const tagPersonName = config.personToTag;
+      let message = config['message'];
+      let regexPath = config['regexPath'];
 
-        if (message === undefined) {
-            message = 'would you be so kind to review the following code changes?';
-        }
+      if (message === undefined) {
+        message = 'would you be so kind to review the following code changes?';
+      }
 
-        const result = await checkFiles(context, regexPath);
+      const result = await checkFiles(context, regexPath);
 
-        await context.octokit.rest.pulls.createReview({
-            ...getRepoSettings(context),
-            event: 'REQUEST_CHANGES',
-            body: createCommentText(result, tagPersonName, message)
-        });
+      await context.octokit.rest.pulls.createReview({
+        ...getRepoSettings(context),
+        event: 'REQUEST_CHANGES',
+        body: createCommentText(result, tagPersonName, message)
+      });
 
-        async function checkFiles(context, source, regexPath) {
-            let per_page = 100
+      async function checkFiles(context, source, regexPath) {
+        let per_page = 100
 
-            const fileMatchRegex = new RegExp(regexPath)
+        const fileMatchRegex = new RegExp(regexPath)
 
-            const filesThatNeedReview = [];
+        const filesThatNeedReview = [];
 
-            for await (const changedFiles of context.octokit.paginate.iterator(
-                context.octokit.pulls.listFiles,
-                {...getRepoSettings(context, source), per_page}
-            )) {
-                for (let file of changedFiles.data) {
+        for await (const changedFiles of context.octokit.paginate.iterator(
+          context.octokit.pulls.listFiles,
+          {...getRepoSettings(context, source), per_page}
+        )) {
+          for (let file of changedFiles.data) {
 
-                    let fileName = file.filename;
-                    if (fileMatchRegex.test(fileName)) {
-                        filesThatNeedReview.push(fileName)
-                    }
-                }
+            let fileName = file.filename;
+            if (fileMatchRegex.test(fileName)) {
+              filesThatNeedReview.push(fileName)
             }
-            return filesThatNeedReview;
+          }
         }
+        return filesThatNeedReview;
+      }
 
-        function getRepoSettings(context) {
-            return {
-                owner: context.payload.repository.full_name.split('/')[0],
-                repo: context.payload.repository.name,
-                pull_number : context.payload.pull_request.number
-            }
+      function getRepoSettings(context) {
+        return {
+          owner: context.payload.repository.full_name.split('/')[0],
+          repo: context.payload.repository.name,
+          pull_number: context.payload.pull_request.number
         }
+      }
 
-        function createCommentText(result, tagPersonName) {
-            let comment = `@${tagPersonName} ${message} \n`;
-            result.forEach(result => {
-                comment = comment.concat(`- [ ] ${result} \n`)
-            })
-            return comment.slice(0, -1);
-        }
+      function createCommentText(result, tagPersonName) {
+        let comment = `@${tagPersonName} ${message} \n`;
+        result.forEach(result => {
+          comment = comment.concat(`- [ ] ${result} \n`)
+        })
+        return comment.slice(0, -1);
+      }
 
     }
   )
